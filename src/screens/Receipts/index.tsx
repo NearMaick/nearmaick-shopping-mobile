bbc9a152677c6d50a70f41dfa019b35cf13f670e
Maick Souza <maick_a_s@msn.com>
2021-12-26 10:42:01 -0300

    feat(mobile): list image preview text

--- src/screens/Receipts/index.tsx
+++ src/screens/Receipts/index.tsx
@@ -12,6 +12,7 @@ import { photosData } from '../../utils/photo.data';
 export function Receipts() {
   const [photos, setPhotos] = useState<FileProps[]>([])
   const [photoSelected, setPhotoSelected] = useState('')
+  const [photoInfo, setPhotoInfo] = useState('')
 
   useEffect(() => {
     storage().ref('images').list().then(result => {
@@ -31,6 +32,9 @@ export function Receipts() {
   async function handleShowImage(path: string) {
     const urlImage = await storage().ref(path).getDownloadURL();
     setPhotoSelected(urlImage)
+
+    const info = await storage().ref(path).getMetadata()
+    setPhotoInfo(`Upload realizado em ${info.timeCreated}`)
   }
 
   return (
@@ -40,7 +44,7 @@ export function Receipts() {
       <Photo uri={photoSelected} />
 
       <PhotoInfo>
-        Informações da foto
+        {photoInfo}
       </PhotoInfo>
 
       <FlatList
