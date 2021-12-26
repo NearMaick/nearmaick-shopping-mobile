8d731714fed25db4ecf0088e4414d1992fac374a
Maick Souza <maick_a_s@msn.com>
2021-12-26 10:13:23 -0300

    feat(mobile): upload image progress

--- src/screens/Upload/index.tsx
+++ src/screens/Upload/index.tsx
@@ -11,6 +11,8 @@ import { Alert } from 'react-native';
 
 export function Upload() {
   const [image, setImage] = useState('');
+  const [bytesTransferred, setBytesTransferred] = useState('');
+  const [progress, setProgress] = useState('0');
 
   async function handlePickImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -32,15 +34,21 @@ export function Upload() {
     const fileName = new Date().getTime();
     const reference = storage().ref(`/images/${fileName}.png`);
 
-    reference
-    .putFile(image)
-    .then(() => Alert.alert('Upload concluído!'))
-    .catch((error) => console.error(error))
+    const uploadTask = reference.putFile(image);
+
+    uploadTask.on('state_changed', taskSnapshot => {
+      const percent = ((taskSnapshot.bytesTransferred / taskSnapshot.totalBytes) * 100).toFixed(0);
+      setProgress(percent);
+
+      setBytesTransferred(`${taskSnapshot.bytesTransferred} transferido de ${taskSnapshot.totalBytes}`)
+    })
+
+    uploadTask.then(() => Alert.alert('Upload concluído!'))
   }
 
   return (
     <Container>
-      <Header title="Lista de compras" />
+      <Header title="Upload de fotos" />
 
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
@@ -51,11 +59,11 @@ export function Upload() {
         />
 
         <Progress>
-          0%
+          {progress}%
         </Progress>
 
         <Transferred>
-          0 de 100 bytes transferido
+          {bytesTransferred}
         </Transferred>
       </Content>
     </Container>
