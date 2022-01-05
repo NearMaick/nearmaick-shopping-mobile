685dff74af0317339281acf5ad42c1069ceec24f
Maick Souza <maick_a_s@msn.com>
2022-01-05 11:40:50 -0300

    chore(mobile): SignInAnonimously

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
 
 import { AppRoutes } from './app.routes';
 import { SignIn } from '../screens/SignIn';
 
 export function Routes() {
+  async function handleSignInAnonimously() {
+    const { user } = await auth().signInAnonymously()
+    console.log(user)
+  }
+
   return (
     <NavigationContainer>
-      <AppRoutes />
+      <SignIn />
     </NavigationContainer>
   )
 }
